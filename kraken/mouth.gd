861f7001d1b2d3ea99f3fac875bf2924363daa6c
Damien Kusters <damien.kusters@ziggo.nl>
2024-08-18 23:50:59 +0200

    feat: new assets

--- kraken/mouth.gd
+++ kraken/mouth.gd
@@ -1,10 +1,14 @@
 extends Node2D
 
+@onready var mouth_open = preload("res://kraken/mouth/mouth_open.png")
+@onready var mouth_closed = preload("res://kraken/mouth/mouth_rest.png")
+
 @onready var blood_particles = preload("res://particles/blood_particles.tscn")
 
 func _on_feed_area_area_entered(area: Area2D) -> void:
 	if area.is_in_group("entity"):
 		var entity = area.get_parent() as Entity
+		$Mouth.texture = mouth_closed
 		entity.eat()
 		spawn_particles(blood_particles, entity)
 		get_parent().entity_eaten.emit()
@@ -14,3 +18,11 @@ func spawn_particles(particles: PackedScene, entity: Entity):
 	p.color = entity.particle_color_eat
 	add_child(p)
 	
+func _update_mouth_texture(area: Area2D) -> void:
+	$Mouth.texture = mouth_closed if !_entity_is_nearby() else mouth_open
+
+func _entity_is_nearby():
+	for area in $OpenMouthArea.get_overlapping_areas():
+		if area.is_in_group("entity"):
+			return true
+	return false
--- kraken/tentacle/tentacle.gd
+++ kraken/tentacle/tentacle.gd
@@ -1,13 +1,18 @@
 extends Node2D
 
+@onready var tentacle_open = preload("res://kraken/tentacle/tentacle_open.png")
+@onready var tentacle_closed = preload("res://kraken/tentacle/tentacle_close.png")
+
 const DRAG_SPEED = .2;
 
 var grabbed_entity
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	_tween_position(get_global_mouse_position() - get_parent().position, grabbed_entity)
 	if grabbed_entity != null:
 		grabbed_entity.global_position = global_position
+	var angle_to_player = global_position.direction_to($"../TentaclePoint".position).angle()
+	rotation = move_toward(rotation, angle_to_player, delta)
 
 var _tween: Tween
 func _tween_position(_global_mouse_position: Vector2, _grabbed_entity):
@@ -18,8 +23,10 @@ func _tween_position(_global_mouse_position: Vector2, _grabbed_entity):
 func _input(_event):
 	if _event is InputEventMouseButton and _event.is_action_pressed("grab"):
 		grabbed_entity = grab_entity()
+		$Tentacle.texture = tentacle_closed
 	elif _event is InputEventMouseButton and _event.is_action_released("grab"):
 		grabbed_entity = null
+		$Tentacle.texture = tentacle_open
 		
 func grab_entity() -> Entity:
 	for area in $Grabber.get_overlapping_areas():
